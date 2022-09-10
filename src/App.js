099a79f133ac8852fc1e2434c8e802c730eee1ad
VladyslavGilyuk <vladgilyuk@gmail.com>
2022-09-11 00:11:11 +0300

    Deleted unnecessary files. Comments added.

--- src/App.js
+++ src/App.js
@@ -1,66 +1,73 @@
-import { useState} from 'react';
+import {useState} from 'react';
 import './App.css';
 
 /* eslint no-eval: 0 */
 
 const App = () => {
-  
+  /*sets the state of displayed result*/
   const [result, setResult] = useState("")
 
-  const  handleClick = (e) => {
+  /*concatenates value of pressed btn with display result*/
+  const handleClick = (e) => {
       setResult(result.concat(e.target.innerText))
     }
+
+  /*clears the result*/
   const clear = () => {
       setResult("")
     }
+
+    /*deletes last entered value*/
   const backspace = () => {
       setResult(result.slice(0, -1))
     }
 
+  /*does math calculation*/
   const calculate = () => {
-      try {
-        if (result === "") {
-          setResult("Empty!");
+    try {
+      if (result === "") {
+        setResult("Empty!");
+        setTimeout( () => setResult(""), 
+          1000
+        ); 
+      } else {
+          setResult(eval(result).toString()) ;
+      }} catch {
+          setResult("Error")
           setTimeout( () => setResult(""), 
             1000
           );
-          
-        } else {
-          setResult(eval(result).toString()) ;
-        }} catch {
-        setResult("Error")
-        setTimeout( () => setResult(""), 
-            1000
-          );
-      }
-    }
+         }
+  }
   
 
   return (
     <>
       <div class="container">
+            {/*Display Section*/}
             <div id="display">{result}</div>
+            {/*Buttons Section*/}
             <div class="buttons">
-                <div class="button " id="all-clear" onClick={clear}>AC</div>
-                <div class="button operator" onClick={handleClick}>/</div>
-                <div class="button operator" onClick={handleClick}>*</div>
-                <div  class="button number" onClick={handleClick}>7</div>
-                <div class="button number" onClick={handleClick}>8</div>
-                <div class="button number" onClick={handleClick}>9</div>
-                <div class="button operator" id="backspace" onClick={backspace}>←</div>
-                <div class="button number" onClick={handleClick}>4</div>
-                <div class="button number" onClick={handleClick}>5</div>
-                <div class="button number" onClick={handleClick}>6</div>
-                <div class="button operator" onClick={handleClick}>-</div>
-                <div class="button number" onClick={handleClick}>1</div>
-                <div class="button number" onClick={handleClick}>2</div>
-                <div class="button number" onClick={handleClick}>3</div>
-                <div class="button operator" onClick={handleClick}>+</div>
-                <div class="button number" onClick={handleClick}>0</div>
-                <div class="button number" onClick={handleClick}>.</div>
-                <div class="button" id="equal" onClick={calculate}>=</div>
+              <div class="button " id="all-clear" onClick={clear}>AC</div>
+              <div class="button operator" onClick={handleClick}>/</div>
+              <div class="button operator" onClick={handleClick}>*</div>
+              <div  class="button number" onClick={handleClick}>7</div>
+              <div class="button number" onClick={handleClick}>8</div>
+              <div class="button number" onClick={handleClick}>9</div>
+              <div class="button operator" id="backspace" onClick={backspace}>←</div>
+              <div class="button number" onClick={handleClick}>4</div>
+              <div class="button number" onClick={handleClick}>5</div>
+              <div class="button number" onClick={handleClick}>6</div>
+              <div class="button operator" onClick={handleClick}>-</div>
+              <div class="button number" onClick={handleClick}>1</div>
+              <div class="button number" onClick={handleClick}>2</div>
+              <div class="button number" onClick={handleClick}>3</div>
+              <div class="button operator" onClick={handleClick}>+</div>
+              <div class="button number" onClick={handleClick}>0</div>
+              <div class="button number" onClick={handleClick}>.</div>
+              <div class="button" id="equal" onClick={calculate}>=</div>
             </div>
-        </div>
+      </div>
     </>
   );
 }
